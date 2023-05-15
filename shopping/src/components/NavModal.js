1d67a4ff5186852aab5f223538d081853f12f404
yeon0226 <w01086248649@gmail.com>
2023-05-15 19:40:00 +0900

    header modal구성

---
+++ shopping/src/components/NavModal.js
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+import React from 'react'
+
+export default function NavModal(){
+    return (
+        <div className='navmodal'>
+            <p className='modal-list'>위정연님, 안녕하세요!</p>
+            <p className='modal-list'> 📄 상품리스트 페이지</p>
+            <p className='modal-list'>⭐️ 북마크 페이지</p>
+        </div>
+    )
+}
