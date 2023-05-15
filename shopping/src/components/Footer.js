768800de934e1e19f5fea825b46f62ecd7f1ba3f
yeon0226 <w01086248649@gmail.com>
2023-05-15 12:36:57 +0900

    footer완성

---
+++ shopping/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <footer>
+            <p>개인정보 처리방침 | 이용 약관</p>
+            <p>All rights reserved @ Codestates</p>
+        </footer>
+    )
+}
+
+export default Footer
