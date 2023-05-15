d054126c1861d7d54529b399016166afb4cbb73d
yeon0226 <w01086248649@gmail.com>
2023-05-15 19:09:23 +0900

    header 구성

---
+++ shopping/src/components/Nav.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import NavModal from './NavModal';
+
+function Nav() {
+    // const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <div className="Nav">
+            <div className='title-container'>
+                <div className='title-logo'>
+                <img className='logo' src={process.env.PUBLIC_URL + 'logo.png'} alt="Logo" />
+                <h2>COZ Shopping</h2>
+                </div>
+                {/* <div className='hamburger-logo' onClick={() =>{ setIsOpen(!isOpen)}}> */}
+                <img className='hamburger' src={process.env.PUBLIC_URL + 'hamburger.png'} alt="Hamburger" />
+                </div>
+            </div>
+            // {isOpen ? <NavModal /> : null}
+        // </div>
+    )
+}
+
+export default Nav;
