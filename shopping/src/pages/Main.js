b46d462ce3dd0f9373cbdb639c81d8609e0b6f28
yeon0226 <w01086248649@gmail.com>
2023-05-17 13:16:19 +0900

    main page css +코드수정

--- shopping/src/pages/Main.js
+++ shopping/src/pages/Main.js
@@ -1,29 +1,25 @@
     import React, { useEffect, useState } from "react";
+    import "./Main.css";
     import ProductList from "./ProductList";
+
 
     const Main = () => {
     const [productItems, setProductItems] = useState([]);
 
-    useEffect(() => {
-        const fetchData = async () => {
-        try {
-            const response = await fetch(
-            "http://cozshopping.codestates-seb.link/api/v1/products?count=4"
-            );
-            const data = await response.json();
-            setProductItems(data);
-        } catch (error) {
-            console.error("Error fetching data:", error);
-        }
-        };
-
-        fetchData();
-    }, []);
+        useEffect(() => {
+            fetch("http://cozshopping.codestates-seb.link/api/v1/products?count=4")
+            .then((res) => res.json())
+            .then((data) => setProductItems(data));
+        }, []);
 
     return (
-        <div>
-        <h2>상품리스트</h2>
-        <ProductList productItems={productItems} />
+        <div className='wrap'>
+            <div className='content'>
+                <div className='product-list'>
+                <h2 className='product-title'>상품리스트</h2>
+                <ProductList productItems={productItems} />
+                </div>
+            </div>
         </div>
     );
     };
